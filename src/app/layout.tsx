7318406d84822936a612de07b54af62be3a4b8e7
avasconcelos114 <andre.onogoro@gmail.com>
2025-07-27 00:13:25 +0300

    chore: updating yarn lock

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,7 @@ export default function RootLayout({
         />
         <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
-          rel='preload'
-          as='style'
-          crossOrigin='anonymous'
+          rel='stylesheet'
         />
       </head>
       <body
